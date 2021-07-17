588a769c25a7eb23e07a040e4387ec3b138fdab0
true_mogician <925269713@qq.com>
2021-07-18 00:32:37 +0800

    feat: audit and delete requests

---
+++ Kingdee/Requests/AuditRequest.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable StringLiteralTypo
+using System.Collections.Generic;
+using Kingdee.Forms;
+using Newtonsoft.Json;
+using Shared.JsonConverters;
+
+namespace Kingdee.Requests {
+	public class AuditRequest<T> : DeleteRequest<T> where T : FormBase {
+		public AuditRequest(string idName, params T[] entities) : base(idName, entities) { }
+
+		/// <summary>
+		///     交互标志集合
+		/// </summary>
+		[JsonProperty("InterationFlags")]
+		[JsonConverter(typeof(StringCollectionConverter), ';')]
+		public List<string> InteractionFlags { get; set; } = new();
+	}
+}
---
+++ Kingdee/Requests/DeleteRequest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Kingdee.Converters;
+using Kingdee.Forms;
+using Newtonsoft.Json;
+using Shared.Exceptions;
+using Shared.JsonConverters;
+
+namespace Kingdee.Requests {
+	public class DeleteRequest<T> : RequestBase where T : FormBase {
+		public DeleteRequest(string idName, params T[] entities) {
+			IdProperty = typeof(T).GetProperty(idName, typeof(string));
+			if (IdProperty is null)
+				throw new TypeReflectionException(typeof(T), $"Property \"{idName}\" with string type not found");
+			Entities = entities.ToList();
+		}
+
+		[JsonIgnore]
+		public PropertyInfo IdProperty { get; }
+
+		[JsonIgnore]
+		public List<T> Entities { get; }
+
+		/// <summary>
+		///     创建者组织内码
+		/// </summary>
+		[JsonProperty("CreateOrgId")]
+		public int CreatorOrgId { get; set; }
+
+		/// <summary>
+		///     单据编码集合
+		/// </summary>
+		[JsonProperty("Numbers")]
+		public List<object> Numbers { get; set; } = new();
+
+		/// <summary>
+		///     单据内码集合
+		/// </summary>
+		[JsonProperty("Ids")]
+		[JsonConverter(typeof(StringCollectionConverter), ',')]
+		public IEnumerable<string> Ids => Entities.Select(entity => IdProperty.GetValue(entity) as string);
+
+		/// <summary>
+		///     是否启用网控
+		/// </summary>
+		[JsonProperty("NetworkCtrl")]
+		[JsonConverter(typeof(BoolConverter))]
+		public bool? NetworkControl { get; set; }
+	}
+}
