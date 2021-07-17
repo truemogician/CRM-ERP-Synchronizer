170f7a687059846c7780e5862d33e9626c3d0118
true_mogician <925269713@qq.com>
2021-07-17 18:42:56 +0800

    test: TheFirstFarm.Test

---
+++ TheFirstFarm.Test/Request.Test.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace TheFirstFarm.Test {
+	public class Tests {
+		[SetUp]
+		public void Setup() {
+		}
+
+		[Test]
+		public void Test1() {
+			Assert.Pass();
+		}
+	}
+}
