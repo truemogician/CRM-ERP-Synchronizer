7ef0a741067f00914d349abe41a4cac1f1d116b2
true_mogician <925269713@qq.com>
2021-07-18 01:07:19 +0800

    feat: creation responses

---
+++ Kingdee/Responses/BatchSaveResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Kingdee.Responses {
+	public class BatchSaveResponse : BasicResponse {
+		[JsonProperty("NeedReturnData")]
+		public List<object> DataToReturn { get; set; }
+	}
+}
---
+++ Kingdee/Responses/SaveResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Kingdee.Responses {
+	public class SaveResponse : BatchSaveResponse {
+		[JsonProperty("Id")]
+		public string Id { get; set; }
+
+		[JsonProperty("Number")]
+		public string Number { get; set; }
+	}
+}
