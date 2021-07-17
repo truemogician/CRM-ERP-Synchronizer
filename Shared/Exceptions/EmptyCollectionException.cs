b03e3774841579e80a5058e83099f2aaccdec520
true_mogician <925269713@qq.com>
2021-07-17 18:37:14 +0800

    feat: custom exceptions

---
+++ Shared/Exceptions/EmptyCollectionException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Shared.Exceptions {
+	public class EmptyCollectionException : Exception {
+		public EmptyCollectionException(string message = null, Exception innerException = null) : base(message, innerException) { }
+	}
+}
