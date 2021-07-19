bfe7c2bc1a7a23b3aa3f7d0371735753c6a98eec
true_mogician <925269713@qq.com>
2021-07-19 23:41:08 +0800

    feat: add enums

---
+++ Kingdee/Forms/Enums/RecordStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Kingdee.Forms.Enums {
+	[JsonConverter(typeof(StringEnumConverter))]
+	public enum RecordStatus : byte {
+		/// <summary>
+		/// 暂存
+		/// </summary>
+		[EnumMember(Value = "Z")]
+		Drafted,
+
+		/// <summary>
+		/// 创建
+		/// </summary>
+		[EnumMember(Value = "A")]
+		Created,
+
+		/// <summary>
+		/// 审核中
+		/// </summary>
+		[EnumMember(Value = "B")]
+		Auditing,
+
+		/// <summary>
+		/// 已审核
+		/// </summary>
+		[EnumMember(Value = "C")]
+		Audited,
+
+		/// <summary>
+		/// 重新审核
+		/// </summary>
+		[EnumMember(Value = "D")]
+		Reauditing
+	}
+}
