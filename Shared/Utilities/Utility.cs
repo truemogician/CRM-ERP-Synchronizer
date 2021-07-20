2eb94c06e4e7ba5f35b19d6c3be15b31e48d4a57
true_mogician <925269713@qq.com>
2021-07-21 01:57:14 +0800

    fix: VerifyInheritance

--- Shared/Utilities/Utility.cs
+++ Shared/Utilities/Utility.cs
@@ -8,9 +8,9 @@ namespace Shared.Utilities {
 	public static class Utility {
 		private static readonly DataAnnotationsValidator.DataAnnotationsValidator Validator = new();
 
-		public static void VerifyInheritance(Type derived, Type super) {
-			if (!derived.IsSubclassOf(super))
-				throw new TypeException(derived, $"Type \"{derived.FullName}\" should derive from \"{super.FullName}\"");
+		public static void VerifyInheritance(Type derived, Type @base) {
+			if (!derived.IsAssignableTo(@base))
+				throw new TypeException(derived, $"Type \"{derived.FullName}\" should derive from \"{@base.FullName}\"");
 		}
 
 		public static List<ValidationResult> Validate<T>(T obj, bool recursive = false) {
@@ -25,7 +25,7 @@ namespace Shared.Utilities {
 		public static void ValidatedOrThrow<T>(T obj) {
 			var results = Validate(obj);
 			if (results.Count > 0)
-				throw new ValidationFailedException(results);
+				throw new ValidationFailedException(obj, results);
 		}
 
 		public static T Construct<T>(params object[] parameters) => (T)typeof(T).Construct(parameters);
