4046566d44320e5993690223a56f9aff74935490
true_mogician <925269713@qq.com>
2021-07-24 13:35:07 +0800

    fix: master-slave relationship

--- TheFirstFarm/Models/FXiaoKe/CustomerAddress.cs
+++ TheFirstFarm/Models/FXiaoKe/CustomerAddress.cs
@@ -7,22 +7,26 @@ namespace TheFirstFarm.Models.FXiaoKe {
 	/// <summary>
 	///     客户地址
 	/// </summary>
-	[Model("AccountAddrObj")]
+	[Model("AccountAddrObj", SubjectTo = typeof(Customer))]
 	public class CustomerAddress : ModelBase {
 		/// <summary>
 		///     地址编号
 		/// </summary>
 		[JsonProperty("name")]
 		[Key]
-		[RegularExpression(@"Addr\.\d{4}-\d{2}-\d{2}_\d{6,}")]
 		[Required]
+		[RegularExpression(@"Addr\.\d{4}-\d{2}-\d{2}_\d{6,}")]
 		public string Id { get; set; }
+
+		[JsonProperty("account_id")]
+		[MasterKey]
+		[Required]
+		public string CustomerId { get; set; }
 
 		/// <summary>
 		///     地区定位
 		/// </summary>
 		[JsonProperty("regional_location")]
-		[Required]
 		public string Location { get; set; }
 
 		/// <summary>
@@ -30,13 +34,12 @@ namespace TheFirstFarm.Models.FXiaoKe {
 		/// </summary>
 		[JsonProperty("is_ship_to_add")]
 		[Required]
-		public string IsShippingAddress { get; set; }
+		public bool IsShippingAddress { get; set; }
 
 		/// <summary>
 		///     联系方式
 		/// </summary>
 		[JsonProperty("contact_way")]
-		[Required]
 		public string ContactWay { get; set; }
 	}
 }
--- TheFirstFarm/Models/FXiaoKe/CustomerFinancialInfo.cs
+++ TheFirstFarm/Models/FXiaoKe/CustomerFinancialInfo.cs
@@ -7,7 +7,7 @@ namespace TheFirstFarm.Models.FXiaoKe {
 	/// <summary>
 	///     客户财务信息
 	/// </summary>
-	[Model("AccountFinInfoObj")]
+	[Model("AccountFinInfoObj", SubjectTo = typeof(Customer))]
 	public class CustomerFinancialInfo : ModelBase {
 		/// <summary>
 		///     发票抬头
@@ -17,25 +17,27 @@ namespace TheFirstFarm.Models.FXiaoKe {
 		[Required]
 		public string Title { get; set; }
 
+		[JsonProperty("account_id")]
+		[MasterKey]
+		[Required]
+		public string CustomerId { get; set; }
+
 		/// <summary>
 		///     纳税识别号
 		/// </summary>
 		[JsonProperty("tax_id")]
-		[Required]
 		public string TaxpayerId { get; set; }
 
 		/// <summary>
 		///     开户银行
 		/// </summary>
 		[JsonProperty("account_bank")]
-		[Required]
 		public string OpeningBank { get; set; }
 
 		/// <summary>
 		///     开户账户
 		/// </summary>
 		[JsonProperty("account_bank_no")]
-		[Required]
 		public string BankAccount { get; set; }
 
 		/// <summary>
