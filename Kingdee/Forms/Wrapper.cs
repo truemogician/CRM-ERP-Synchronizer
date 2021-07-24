781ea823051987bc987e168196be4c29b79af723
true_mogician <925269713@qq.com>
2021-07-24 13:30:57 +0800

    refactor: WrapperBase

--- Kingdee/Forms/Wrapper.cs
+++ Kingdee/Forms/Wrapper.cs
@@ -1,23 +1,33 @@
-﻿using Newtonsoft.Json;
+﻿using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
 
 namespace Kingdee.Forms {
 	public class NumberWrapper : NumberWrapper<string> { }
 
-	public class NumberWrapper<T> {
+	public class NumberWrapper<T> : WrapperBase<T> {
 		[JsonProperty("FNumber")]
 		public T Number { get; set; }
 
-		public static implicit operator T(NumberWrapper<T> self) => self.Number;
+		//public static implicit operator T(NumberWrapper<T> self) => self.Number;
 		public static implicit operator NumberWrapper<T>(T number) => new() {Number = number};
+		protected override string ValueName => nameof(Number);
 	}
 
 	public class NameWrapper : NameWrapper<string> { }
 
-	public class NameWrapper<T> {
+	public class NameWrapper<T> : WrapperBase<T> {
 		[JsonProperty("FName")]
 		public T Name { get; set; }
 
-		public static implicit operator T(NameWrapper<T> self) => self.Name;
+		//public static implicit operator T(NameWrapper<T> self) => self.Name;
 		public static implicit operator NameWrapper<T>(T number) => new() {Name = number};
+		protected override string ValueName => nameof(Name);
+	}
+
+	public abstract class WrapperBase<T> {
+		protected abstract string ValueName { get; }
+
+		public static implicit operator T(WrapperBase<T> self) => (T)self.GetType().GetMember(self.ValueName).Single().GetValue(self);
 	}
 }
