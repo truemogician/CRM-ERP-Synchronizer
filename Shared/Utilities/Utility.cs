e21fd05ab2bc75d377457a3916fbad17b4b7618c
true_mogician <925269713@qq.com>
2021-07-24 13:36:43 +0800

    refactor: Utility

--- Shared/Utilities/Utility.cs
+++ Shared/Utilities/Utility.cs
@@ -9,10 +9,10 @@ namespace Shared.Utilities {
 
 		public static void VerifyInheritance(Type derived, Type @base) {
 			if (!derived.IsAssignableTo(@base))
-				throw new TypeException(derived, $"Type \"{derived.FullName}\" should derive from \"{@base.FullName}\"");
+				throw new InvariantTypeException(@base, derived);
 		}
 
-		public static List<ValidationResult> Validate<T>(T obj, bool recursive = false) {
+		public static List<ValidationResult> Validate<T>(T obj, bool recursive = true) {
 			var result = new List<ValidationResult>();
 			if (recursive)
 				Validator.TryValidateObjectRecursive(obj, result);
@@ -21,8 +21,8 @@ namespace Shared.Utilities {
 			return result;
 		}
 
-		public static void ValidatedOrThrow<T>(T obj) {
-			var results = Validate(obj);
+		public static void ValidatedOrThrow<T>(T obj, bool recursive = true) {
+			var results = Validate(obj, recursive);
 			if (results.Count > 0)
 				throw new ValidationFailedException(obj, results);
 		}
