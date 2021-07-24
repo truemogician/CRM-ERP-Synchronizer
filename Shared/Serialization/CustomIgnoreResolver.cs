1a4bc8fe9a862aa8234a740afb9a19d275feb4b9
true_mogician <925269713@qq.com>
2021-07-24 13:29:49 +0800

    feat: CustomIgnoreResolver

---
+++ Shared/Serialization/CustomIgnoreResolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Shared.Serialization {
+	public class CustomIgnoreResolver : DefaultContractResolver {
+		public CustomIgnoreResolver() : this(Array.Empty<Type>()) { }
+		public CustomIgnoreResolver(params Type[] ignoreAttributes) => IgnoreAttributes = ignoreAttributes.ToHashSet();
+		public HashSet<Type> IgnoreAttributes { get; }
+
+		protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization) {
+			var properties = base.CreateProperties(type, memberSerialization);
+			foreach (var prop in properties) {
+				var attrs = prop.AttributeProvider!.GetAttributes(true);
+				if (!prop.Ignored && attrs.Any(attr => IgnoreAttributes.Contains(attr.GetType())))
+					prop.Ignored = true;
+			}
+			return properties;
+		}
+	}
+}
