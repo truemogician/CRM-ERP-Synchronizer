ac2f7c0af79eb8f03b2bb461cf2cdbd3affd0160
true_mogician <925269713@qq.com>
2021-07-31 00:23:43 +0800

    feat: remove useless fields

--- TheFirstFarm/Models/Kingdee/ReturnOrder.cs
+++ TheFirstFarm/Models/Kingdee/ReturnOrder.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 using Kingdee.Forms;
 using Kingdee.Forms.Enums;
 using Newtonsoft.Json;
@@ -15,132 +14,34 @@ namespace TheFirstFarm.Models.Kingdee {
 	public class ReturnOrder : FormBase {
 		[JsonProperty("FID")]
 		[Key]
-		[JsonInclude]
 		public int Id { get; set; }
 
 		[JsonProperty("FBillNo")]
-		[JsonInclude]
 		public string Number { get; set; }
 
 		[JsonProperty("FBussinessType")]
-		[JsonInclude]
 		public BusinessType BusinessType { get; set; }
 
 		[JsonProperty("FDate")]
-		[JsonInclude]
 		public DateTime? Date { get; set; }
 
 		[JsonProperty("FRetcustId")]
-		[JsonInclude]
-		public NumberWrapper CustomerId { get; set; }
+		public NumberWrapper CustomerNumber { get; set; }
 
 		[JsonProperty("FSalesManId")]
-		[JsonInclude]
-		public NumberWrapper SalesmanId { get; set; }
+		public NumberWrapper SalesmanNumber { get; set; }
 
 		[JsonProperty("FReturnReason")]
-		[JsonInclude]
 		public NumberWrapper ReturnReason { get; set; }
 
-		[JsonProperty("FEntity")]
-		[SubForm]
-		[JsonInclude]
-		public List<ReturnOrderDetail> Details { get; set; }
-
 		[JsonProperty("FDocumentStatus")]
-		[JsonInclude]
 		public RecordStatus RecordStatus { get; set; }
 
 		[JsonProperty("FApproveDate")]
-		[JsonInclude]
 		public DateTime? AuditDate { get; set; }
 
-		[JsonProperty("FBillTypeID")]
-		public NumberWrapper FBillTypeID { get; set; }
-
-		[JsonProperty("FSaleOrgId")]
-		public NumberWrapper FSaleOrgId { get; set; }
-
-		[JsonProperty("FSaledeptid")]
-		public NumberWrapper FSaledeptid { get; set; }
-
-		[JsonProperty("FHeadLocId")]
-		public NumberWrapper FHeadLocId { get; set; }
-
-		[JsonProperty("FCorrespondOrgId")]
-		public NumberWrapper FCorrespondOrgId { get; set; }
-
-		[JsonProperty("FSaleGroupId")]
-		public NumberWrapper FSaleGroupId { get; set; }
-
-		[JsonProperty("FRetorgId")]
-		public NumberWrapper FRetorgId { get; set; }
-
-		[JsonProperty("FRetDeptId")]
-		public NumberWrapper FRetDeptId { get; set; }
-
-		[JsonProperty("FStockerGroupId")]
-		public NumberWrapper FStockerGroupId { get; set; }
-
-		[JsonProperty("FStockerId")]
-		public NameWrapper FStockerId { get; set; }
-
-		[JsonProperty("FDescription")]
-		public string FDescription { get; set; }
-
-		[JsonProperty("FReceiveCusId")]
-		public NumberWrapper FReceiveCusId { get; set; }
-
-		[JsonProperty("FReceiveAddress")]
-		public string FReceiveAddress { get; set; }
-
-		[JsonProperty("FSettleCusId")]
-		public NumberWrapper FSettleCusId { get; set; }
-
-		[JsonProperty("FReceiveCusContact")]
-		public NameWrapper FReceiveCusContact { get; set; }
-
-		[JsonProperty("FPayCusId")]
-		public NumberWrapper FPayCusId { get; set; }
-
-		[JsonProperty("FOwnerTypeIdHead")]
-		public string FOwnerTypeIdHead { get; set; }
-
-		[JsonProperty("FOwnerIdHead")]
-		public NumberWrapper FOwnerIdHead { get; set; }
-
-		[JsonProperty("SubHeadEntity")]
-		public SubHeadEntity SubHeadEntity { get; set; }
-	}
-
-	public class SubHeadEntity {
-		[JsonProperty("FEntryId")]
-		public int FEntryId { get; set; }
-
-		[JsonProperty("FSettleCurrId")]
-		public NumberWrapper FSettleCurrId { get; set; }
-
-		[JsonProperty("FSettleOrgId")]
-		public NumberWrapper FSettleOrgId { get; set; }
-
-		[JsonProperty("FChageCondition")]
-		public NumberWrapper FChageCondition { get; set; }
-
-		[JsonProperty("FSettleTypeId")]
-		public NumberWrapper FSettleTypeId { get; set; }
-
-		[JsonProperty("FLocalCurrId")]
-		public NumberWrapper FLocalCurrId { get; set; }
-
-		[JsonProperty("FExchangeTypeId")]
-		public NumberWrapper FExchangeTypeId { get; set; }
-
-		[JsonProperty("FExchangeRate")]
-		public int FExchangeRate { get; set; }
-	}
-
-	public class FTaxDetailSubEntity {
-		[JsonProperty("FDetailID")]
-		public int FDetailID { get; set; }
+		[JsonProperty("FEntity")]
+		[SubForm]
+		public List<ReturnOrderDetail> Details { get; set; }
 	}
 }
--- TheFirstFarm/Models/Kingdee/ReturnOrderDetail.cs
+++ TheFirstFarm/Models/Kingdee/ReturnOrderDetail.cs
@@ -1,23 +1,21 @@
 ﻿// ReSharper disable StringLiteralTypo
 // ReSharper disable IdentifierTypo
 // ReSharper disable InconsistentNaming
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 using Kingdee.Forms;
 using Newtonsoft.Json;
 using Shared.Serialization;
 using TheFirstFarm.Models.Common;
 
 namespace TheFirstFarm.Models.Kingdee {
-	public class ReturnOrderDetail {
+	[Form()]//TODO
+	public class ReturnOrderDetail : FormBase {
 		/// <summary>
 		///     物料编码
 		/// </summary>
 		[JsonProperty("FMaterialId")]
-		[JsonInclude]
 		[Required]
-		public NumberWrapper MaterialId { get; set; }
+		public NumberWrapper MaterialNumber { get; set; }
 
 		/// <summary>
 		///     物料名称
@@ -46,7 +44,6 @@ namespace TheFirstFarm.Models.Kingdee {
 		///     实退数量
 		/// </summary>
 		[JsonProperty("FREALQTY")]
-		[JsonInclude]
 		[Required]
 		public decimal ReturnAmount { get; set; }
 
@@ -54,164 +51,31 @@ namespace TheFirstFarm.Models.Kingdee {
 		///     含税单价
 		/// </summary>
 		[JsonProperty("FTAXPRICE")]
-		[JsonInclude]
 		public decimal UnitPrice { get; set; }
 
 		/// <summary>
 		///     税率
 		/// </summary>
 		[JsonProperty("FTAXRATE")]
-		[JsonInclude]
 		public decimal TaxRate { get; set; }
 
 		/// <summary>
 		///     金额
 		/// </summary>
 		[JsonProperty("FAMOUNT")]
-		[JsonInclude]
-		public decimal Volumn { get; set; }
+		public decimal Money { get; set; }
 
 		/// <summary>
 		///     退货类型
 		/// </summary>
 		[JsonProperty("FReturnType")]
-		[JsonInclude]
 		[Required]
 		public ReturnTypeWrapper ReturnType { get; set; }
-
-		[JsonProperty("FENTRYID")]
-		public int FENTRYID { get; set; }
-
-		[JsonProperty("FRowType")]
-		public string FRowType { get; set; }
-
-		[JsonProperty("FMapId")]
-		public NumberWrapper FMapId { get; set; }
-
-		[JsonProperty("FParentMatId")]
-		public NumberWrapper FParentMatId { get; set; }
-
-		[JsonProperty("FInventoryQty")]
-		public int FInventoryQty { get; set; }
-
-		[JsonProperty("FQty")]
-		public int FQty { get; set; }
-
-		[JsonProperty("FPRODUCEDATE")]
-		public string FPRODUCEDATE { get; set; }
-
-		[JsonProperty("FExpiryDate")]
-		public string FExpiryDate { get; set; }
-
-		[JsonProperty("FLot")]
-		public NumberWrapper FLot { get; set; }
-
-		[JsonProperty("FPriceBaseQty")]
-		public int FPriceBaseQty { get; set; }
-
-		[JsonProperty("FASEUNITID")]
-		public NumberWrapper FASEUNITID { get; set; }
-
-		[JsonProperty("FDeliverydate")]
-		public string FDeliverydate { get; set; }
-
-		[JsonProperty("FStockId")]
-		public NumberWrapper FStockId { get; set; }
-
-		[JsonProperty("FBOMId")]
-		public NumberWrapper FBOMId { get; set; }
-
-		[JsonProperty("FMtoNo")]
-		public string FMtoNo { get; set; }
-
-		[JsonProperty("FEntryDescription")]
-		public string FEntryDescription { get; set; }
-
-		[JsonProperty("FPriceDiscount")]
-		public int FPriceDiscount { get; set; }
-
-		[JsonProperty("FRmType")]
-		public NumberWrapper FRmType { get; set; }
-
-		[JsonProperty("FIsReturnCheck")]
-		public string FIsReturnCheck { get; set; }
-
-		[JsonProperty("FCheckQty")]
-		public int FCheckQty { get; set; }
-
-		[JsonProperty("FBaseCheckQty")]
-		public int FBaseCheckQty { get; set; }
-
-		[JsonProperty("FQualifiedQty")]
-		public int FQualifiedQty { get; set; }
-
-		[JsonProperty("FBaseQualifiedQty")]
-		public int FBaseQualifiedQty { get; set; }
-
-		[JsonProperty("FUnqualifiedQty")]
-		public int FUnqualifiedQty { get; set; }
-
-		[JsonProperty("FBaseUnqualifiedQty")]
-		public int FBaseUnqualifiedQty { get; set; }
-
-		[JsonProperty("FJunkedQty")]
-		public int FJunkedQty { get; set; }
-
-		[JsonProperty("FBaseJunkedQty")]
-		public int FBaseJunkedQty { get; set; }
-
-		[JsonProperty("FJoinCheckQty")]
-		public int FJoinCheckQty { get; set; }
-
-		[JsonProperty("FBaseJoinCheckQty")]
-		public int FBaseJoinCheckQty { get; set; }
-
-		[JsonProperty("FJoinQualifiedQty")]
-		public int FJoinQualifiedQty { get; set; }
-
-		[JsonProperty("FBaseJoinQualifiedQty")]
-		public int FBaseJoinQualifiedQty { get; set; }
-
-		[JsonProperty("FJoinJunkedQty")]
-		public int FJoinJunkedQty { get; set; }
-
-		[JsonProperty("FBaseJoinJunkedQty")]
-		public int FBaseJoinJunkedQty { get; set; }
-
-		[JsonProperty("FJoinUnqualifiedQty")]
-		public int FJoinUnqualifiedQty { get; set; }
-
-		[JsonProperty("FBaseJoinUnqualifiedQty")]
-		public int FBaseJoinUnqualifiedQty { get; set; }
-
-		[JsonProperty("FStockUnitID")]
-		public NumberWrapper FStockUnitID { get; set; }
-
-		[JsonProperty("FStockQty")]
-		public int FStockQty { get; set; }
-
-		[JsonProperty("FStockBaseQty")]
-		public int FStockBaseQty { get; set; }
-
-		[JsonProperty("FOwnerTypeID")]
-		public string FOwnerTypeID { get; set; }
-
-		[JsonProperty("FOwnerID")]
-		public NumberWrapper FOwnerID { get; set; }
-
-		[JsonProperty("FRefuseFlag")]
-		public string FRefuseFlag { get; set; }
-
-		[JsonProperty("FSOEntryId")]
-		public int FSOEntryId { get; set; }
-
-		[JsonProperty("FTaxDetailSubEntity")]
-		public List<FTaxDetailSubEntity> FTaxDetailSubEntity { get; set; }
 	}
 
 	public class ReturnTypeWrapper : WrapperBase<ReturnType> {
 		[JsonProperty("FNumber")]
-		[Newtonsoft.Json.JsonConverter(typeof(EnumValueConverter), Platform.Kingdee)]
+		[JsonConverter(typeof(EnumValueConverter), Platform.Kingdee)]
 		public ReturnType Value { get; set; }
 
 		protected override string ValueName => nameof(Value);
