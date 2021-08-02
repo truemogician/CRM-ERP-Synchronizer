f4143ed015ba45a98488642465e15dfba67f6505
true_mogician <925269713@qq.com>
2021-08-03 00:32:00 +0800

    fix: reflection constructors

--- Shared/Serialization/NullableConverter.cs
+++ Shared/Serialization/NullableConverter.cs
@@ -8,6 +8,16 @@ namespace Shared.Serialization {
 
 		public NullableConverter(params object[] parameters) => Converter = typeof(TConverter).Construct(parameters) as TConverter;
 
+		#region Reflection Constructors
+		public NullableConverter(object param) : this(new[] {param}) { }
+
+		public NullableConverter(object param1, object param2) : this(new[] {param1, param2}) { }
+
+		public NullableConverter(object param1, object param2, object param3) : this(new[] {param1, param2, param3}) { }
+
+		public NullableConverter(object param1, object param2, object param3, object param4) : this(new[] {param1, param2, param3, param4}) { }
+		#endregion
+
 		public TConverter Converter { get; }
 
 		public override void WriteJson(JsonWriter writer, TValue? value, JsonSerializer serializer) {
